aa7b1c19d2deca6dd5e18e00987b035512bb5cfb
EricRooney <erooneous@gmail.com>
2022-12-10 20:37:03 -0700

    trimmed

--- server/controllers/ScoresController.js
+++ server/controllers/ScoresController.js
@@ -7,10 +7,7 @@ export default class ScoreController {
       .Router()
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
       .get("", this.getAll)
-      .get("/:id", this.getById)
-      .post("", this.create)
-      .put("/:id", this.edit)
-      .delete("/:id", this.delete);
+      .post("", this.create);
   }
 
   async getAll(req, res, next) {
@@ -22,37 +19,10 @@ export default class ScoreController {
     }
   }
 
-  async getById(req, res, next) {
-    try {
-      let data = await scoreService.getById(req.params.id);
-      return res.send(data);
-    } catch (error) {
-      next(error);
-    }
-  }
-
   async create(req, res, next) {
     try {
       let data = await scoreService.create(req.body);
       return res.status(201).send(data);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  async edit(req, res, next) {
-    try {
-      let data = await scoreService.edit(req.params.id, req.body);
-      return res.send(data);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  async delete(req, res, next) {
-    try {
-      await scoreService.delete(req.params.id);
-      return res.send("Successfully Deleted");
     } catch (error) {
       next(error);
     }
--- server/models/Score.js
+++ server/models/Score.js
@@ -4,12 +4,8 @@ const Schema = mongoose.Schema;
 
 const Score = new Schema(
   {
-    make: { type: String, required: true, maxlength: 20 },
-    model: { type: String, required: true },
-    price: { type: Number, required: true },
-    year: { type: Number, max: 2040, min: 1800 },
-    imgUrls: [{ type: String }],
-    description: { type: String, maxlength: 250 },
+    user: { type: String, required: true, maxlength: 25 },
+    score: { type: Number, min: 0, required: true },
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
